c8dc617b20189f4f64df6bf9be4758349509a22d
Abner <abnercabrera28@gmail.com>
2021-04-12 13:43:13 -0400

    commit tree be215545aa65ebf0081be91967ea74e79b54b472

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists, only: [:index, :new, :show, :create, :update, :edit]
+  resources :genres, only: [:index, :new, :show, :create, :update, :edit]
+  resources :songs, only: [:index, :new, :show, :create, :update, :edit]
 end
